58b03145e7c3848e4390031316e3bf91b642332c
Dario Grana <73640145+seismicreservoirmodeling@users.noreply.github.com>
2021-01-18 16:38:06 -0800

    Add files via upload

---
+++ Application3.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 15 09:43:50 2021
+
+@author: dariograna
+"""
+
+from scipy.io import loadmat
+import scipy.spatial.distance
+import matplotlib.pyplot as plt
+import numpy as np
+from Geostats import *
+
+
+#% Application3  (temperature Yellowstone)
+E = loadmat('Data/ElevationData.mat')
+X = E['X']
+Y = E['Y']
+Z = E['Z']
+T = E['T']
+dt = E['dt']
+dx = E['dx']
+dy = E['dy']
+dz = E['dz']
+
+# # available data (100 measurements)
+dcoords = np.hstack([dx,dy])
+nd = dcoords.shape[0]
+# # grid of coordinates of the location to be estimated
+xcoords = np.transpose(np.vstack([X.reshape(-1), Y.reshape(-1)]))
+n = xcoords.shape[0]
+
+# parameters random variable
+tmean = 7.45
+tvar = 0.45
+l = 25
+krigtype = 'exp'
+
+# kriging
+xsk = np.zeros((n, 1))
+for i in range(n):
+    # simple kiging
+    xsk[i,0] = SimpleKriging(xcoords[i,:], dcoords, dt, tmean, tvar, l, krigtype)[0]
+xsk = np.reshape(xsk, X.shape)
+
+# Sequential Gaussian Simulation
+krig = 1
+nsim = 1
+sgsim = np.zeros((X.shape[0], X.shape[1], nsim))
+for i in range(nsim):
+    sim = SeqGaussianSimulation(xcoords, dcoords, dt, tmean, tvar, l, krigtype, krig)
+    sgsim[:,:,i] = np.reshape(sim, (X.shape[0], X.shape[1]))
+
+
+# plot results
+# # plot
+plt.figure(3)
+plt.subplot(311)
+plt.pcolor(X,Y, T)
+plt.scatter(dcoords[:,0], dcoords[:,1], 20, dt, 'o')
+plt.xlabel('X')
+plt.ylabel('Y')
+cbar = plt.colorbar()
+cbar.set_label('Elevation', rotation=270)
+plt.title('Temperature data')
+plt.subplot(312)
+plt.pcolor(X,Y, xsk)
+plt.xlabel('X')
+plt.ylabel('Y')
+cbar = plt.colorbar()
+cbar.set_label('Elevation', rotation=270)
+plt.title('Simple Kriging')
+plt.subplot(313)
+plt.pcolor(X,Y, sgsim[:,:,0])
+plt.xlabel('X')
+plt.ylabel('Y')
+cbar = plt.colorbar()
+cbar.set_label('Temperature', rotation=270)
+plt.title('SGS Realization')
+plt.show()
+
+
